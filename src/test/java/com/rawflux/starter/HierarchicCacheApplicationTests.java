58fbecd4072622d4436f4047ccd7bcab705205c8
danielweiup@gmail.com <danielweiup@gmail.com>
2020-05-16 15:42:23 +0800

    init

---
+++ src/test/java/com/rawflux/starter/HierarchicCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rawflux.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HierarchicCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
